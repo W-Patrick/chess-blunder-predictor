f92aa4a54d0afde4e75cbd145559cf424d145fd5
Patrick Walankiewicz <walankiewicz.p@husky.neu.edu>
2020-04-01 13:48:20 -0400

    simple counting example and add makefile

--- spark-jobs/counting_job.py
+++ spark-jobs/counting_job.py
@@ -1,10 +1,15 @@
+import sys
 from pyspark import SparkContext
-import chess
+import chess.pgn
+import io
 
 if __name__ == '__main__':
+	input_dir = sys.argv[1]
+	output_dir = sys.argv[2]
+
 	sc = SparkContext(appName="PgnCount")
 	sc._jsc.hadoopConfiguration().set("textinputformat.record.delimiter", "\n\n[Event")
-	text_file = sc.textFile("counting-input")
+	text_file = sc.textFile(input_dir)
 
 	# we need to use \n\n[Event as our delimiter because of PGN's specific format
 	# then we need to fix the records so that an individual game is a single record
@@ -15,5 +20,13 @@ if __name__ == '__main__':
 		else:
 			return ''.join(['[Event', record, '\n'])
 
-	res = text_file.map(fix_record)
-	res.saveAsTextFile("output")
+	def analyze_game(record):
+		pgn = io.StringIO(record)
+		game = chess.pgn.read_game(pgn)
+
+		analysis = []
+		analysis.append((game.headers['Result'], 1))
+		return analysis
+
+	res = text_file.map(fix_record).flatmap(analyze_game).reduceByKey(lambda a,b : a + b)
+	res.saveAsTextFile(output_dir)
