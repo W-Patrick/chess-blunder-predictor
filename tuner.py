7119aa0168b5c27366abbf518f37fe80c751439d
Patrick Walankiewicz <walankiewicz.p@husky.neu.edu>
2020-04-23 20:50:14 -0400

    make s3 input url a global var

--- tuner.py
+++ tuner.py
@@ -1,7 +1,7 @@
 import os
 
-training_command = 'python model.py --batch-size {} --epochs 15 --train "https://pwalanki-blunder-predictor-data.s3.us-east-1.amazonaws.com/output-large" --dense-layers {} --dropout {} --learning-rate {} --num-nodes {} --parts 9 --tensorboard True'
-
+training_command = 'python model.py --batch-size {} --epochs 15 --train "{}" --dense-layers {} --dropout {} --learning-rate {} --num-nodes {} --parts 9 --tensorboard True'
+S3_INPUT_URL = 'https://pwalanki-blunder-predictor-data.s3.us-east-1.amazonaws.com/output-large'
 
 # CHANGE THESE VALUES DEPENDING ON
 # WHAT HYPERPARAMETERS YOU WANT TO 
@@ -17,5 +17,5 @@ for dense_layer in dense_layers:
 		for batch_size in batch_sizes:
 			for learning_rate in learning_rates:
 				for dropout in dropouts:
-					command = training_command.format(batch_size, dense_layer, dropout, learning_rate, num_node)
+					command = training_command.format(S3_INPUT_URL, batch_size, dense_layer, dropout, learning_rate, num_node)
 					os.system(command)
